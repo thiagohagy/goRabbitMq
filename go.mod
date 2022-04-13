d81885e361d7cb8260666193ded0882fb2213fad
thiago a. hagy <thiagohagy@hotmail.com>
2022-04-13 07:42:27 -0300

    POC

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module local/goKafka
+module local/goRabbit
 
 go 1.18
 
@@ -20,6 +20,7 @@ require (
 	github.com/pierrec/lz4 v2.6.1+incompatible // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/rcrowley/go-metrics v0.0.0-20201227073835-cf1acfcdf475 // indirect
+	github.com/streadway/amqp v1.0.0 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.1.0 // indirect
 	github.com/xdg-go/stringprep v1.0.2 // indirect
--- main.go
+++ main.go
@@ -1,46 +1,98 @@
 package main
 
 import (
-	"local/goKafka/consumer"
-	"local/goKafka/kafka"
-	"local/goKafka/producer"
+	"fmt"
 	"log"
 	"os"
+	"time"
+
+	"github.com/streadway/amqp"
 )
 
-var brokers = []string{"127.0.0.1:9092"} 
+var amqpServerURL = "amqp://127.0.0.1" 
 var topic string = "device_events"
 var group string = "poc"
 
 func main () {
 	// start kafka
-	var producerDone = make(chan bool)	
-	var consumerDone = make(chan bool)	
+	var forever = make(chan bool)	
 
-	server := &kafka.Server{
-		Topic: topic,
-		Group: group,
-		Brokers: brokers, 
+	connectRabbitMQ, err := amqp.Dial(amqpServerURL)
+    if err != nil {
+        panic(err)
+    }
+    defer connectRabbitMQ.Close()
+
+	channelRabbitMQ, err := connectRabbitMQ.Channel()
+    if err != nil {
+        panic(err)
+    }
+    defer channelRabbitMQ.Close()
+
+	_, err = channelRabbitMQ.QueueDeclare(
+        "GolangRabbitMQTest", // queue name
+        true,            // durable
+        false,           // auto delete
+        false,           // exclusive
+        false,           // no wait
+        nil,             // arguments
+    )
+    if err != nil {
+        panic(err)
+    }
+
+	message := amqp.Publishing{
+		ContentType: "text/plain",
+		Body: []byte("Mensagem de teste"),
 	}
+
 	
-	producer := &producer.Producer{
-		Server: *server,
+	// producer	
+	go func (){
+		for{
+			sendErr := channelRabbitMQ.Publish(
+				"",              // exchange
+				"GolangRabbitMQTest", // queue name
+				false,           // mandatory
+				false,           // immediate
+				message,         // message to publish
+			); 
 		
-	}		
-	go producer.Produce(10, producerDone) 
-
-	worker := &consumer.Worker{
-		Server: *server,
-	}
-	go worker.Consume()
-
-	defer func() { 
-		if err := server.Close(); err != nil {
-			log.Println("Failed to close server", err)
+			if sendErr != nil {
+				fmt.Print(sendErr)
+			} else {
+				log.Printf("-->> Message sent with success")	 
+			}
+	
+			time.Sleep(time.Second * 10)
+			
 		}
 	}()
 
-	<-producerDone
-	<-consumerDone
+
+	// consumer
+	messages, err := channelRabbitMQ.Consume(
+        "GolangRabbitMQTest", // queue name
+        "",              // consumer
+        true,            // auto-ack
+        false,           // exclusive
+        false,           // no local
+        false,           // no wait
+        nil,             // arguments
+    )
+    if err != nil {
+        log.Println(err)
+    }
+
+    // Make a channel to receive messages into infinite loop.
+	go func() {
+        for message := range messages {
+            // For example, show received message in a console.
+            log.Printf("<<-- Received message: %s\n", message.Body)
+        }
+    }()
+	
+
+	<-forever
 	os.Exit(2)		
 }
